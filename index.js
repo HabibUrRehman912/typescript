c8a3016370ddf933ff1dbc744de316bddc94691f
Habib Ur Rehman <34816266+HabibUrRehman912@users.noreply.github.com>
2023-03-30 20:14:58 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+fetch('https://dummyjson.com/products')
+    .then(response => response.json())
+    .then((data) => {
+    const table = document.createElement('table');
+    const headerRow = table.insertRow(0);
+    const keys = Object.keys(data.products[0]);
+    // console.log(data[0].id)
+    keys.forEach(key => {
+        const headerCell = document.createElement('th');
+        headerCell.innerText = key;
+        headerRow.appendChild(headerCell);
+    });
+    data.products.forEach(rowData => {
+        const row = table.insertRow();
+        keys.forEach(key => {
+            const cell = row.insertCell();
+            cell.innerText = rowData[key];
+        });
+    });
+    document.querySelector('#table-container').appendChild(table);
+})
+    .catch(error => console.error(error));
